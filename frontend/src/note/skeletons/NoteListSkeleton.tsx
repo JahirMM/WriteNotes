7ca160cd6143e65727849b1eb373d9bf7f6e0a09
JahirMM <jahirmachuca45@gmail.com>
2024-08-06 19:48:26 -0400

    Modificar las interfaces de carga

--- frontend/src/note/skeletons/NoteListSkeleton.tsx
+++ frontend/src/note/skeletons/NoteListSkeleton.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-
 function NoteListSkeleton() {
   const notes = [];
   for (let index = 0; index < 3; index++) {
     notes.push(
       <div
         key={index}
-        className="bg-colorNote small-note-dimensions p-3 rounded-xl flex flex-col justify-between md:large-note-dimensions"
+        className="bg-colorNote p-3 rounded-xl flex flex-col justify-between large-note-dimensions sm:large-note-dimensions"
       >
         <header>
           <div className="flex justify-between mb-3">
@@ -28,7 +26,7 @@ function NoteListSkeleton() {
     );
   }
   return (
-    <div className="flex gap-3 overflow-auto md:flex-col md:items-center">
+    <div className="flex flex-col gap-3 items-center max-h-[95%] overflow-auto">
       {notes}
     </div>
   );
